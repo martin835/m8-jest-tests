04ed2a7a7f519c5f087247ce3f5e3e101e5574a8
martin835 <64438132+martin835@users.noreply.github.com>
2022-04-26 14:23:10 +0200

    initial set up

--- src/app.js
+++ src/app.js
@@ -1,18 +1,21 @@
-import express from "express"
-import productsRouter from "./products/index.js"
+import express from "express";
+import productsRouter from "./products/index.js";
+import listEndpoints from "express-list-endpoints";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/test', (req, res) => {
-    res.send({ message: 'Hello, World!' })
-})
+app.get("/test", (req, res) => {
+  res.send({ message: "Hello, World!" });
+});
 
-app.use('/products', productsRouter)
+app.use("/products", productsRouter);
 
 // app.listen(3030, () => {
 //     console.log('Server is running on port 3030')
 // })
 
-export default app
+console.table(listEndpoints(app));
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import app from "./app.js";
 import mongoose from "mongoose";
 
 mongoose.connect(process.env.MONGO_URL, () => {
-    console.log("Connected to MongoDB")
-    app.listen(3030, () => {
-        console.log('Server is running on port 3030')
-    })
-})
+  console.log("👌 Connected to MongoDB");
+  app.listen(3030, () => {
+    console.log("✅ Server is running on port 3030");
+  });
+});
